4d5309da1cd7b077a4d046f29ed2602a6b03cc7a
LimeFreezy98 <89162212+LimeFreezy98@users.noreply.github.com>
2026-05-04 09:11:57 -0600

    added game, game-setup, login and state in page folder

---
+++ src/app/components/page/game-setup/game-setup.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-game-setup',
+  imports: [],
+  templateUrl: './game-setup.html',
+  styleUrl: './game-setup.css',
+})
+export class GameSetup {}
