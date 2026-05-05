dd8a37c257b106d502a2d5cd1da58b046e05cd1d
LimeFreezy98 <89162212+LimeFreezy98@users.noreply.github.com>
2026-05-05 08:11:06 -0600

    added sign in google function

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,10 +3,8 @@ import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.css'
 })
-export class App {
-  protected readonly title = signal('angular-capstone-pokemon_api');
-}
+export class App {}
--- src/app/components/page/login/login.ts
+++ src/app/components/page/login/login.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Auth, signInWithPopup, GoogleAuthProvider } from '@angular/fire/auth';
+import { Firestore, doc, getDoc, setDoc } from '@angular/fire/firestore';
+
+
 
 @Component({
   selector: 'app-login',
@@ -6,4 +10,39 @@ import { Component } from '@angular/core';
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
-export class Login {}
+
+export class Login {
+  constructor(private auth: Auth, private firestore: Firestore) {}
+
+  async loginWithGoogle() {
+    const provider = new GoogleAuthProvider();
+    const result = await signInWithPopup(this.auth, provider);
+
+    const user = result.user;
+
+    if (!user) return;
+
+    const userRef = doc(this.firestore, `users/${user.uid}`);
+    const userSnap = await getDoc(userRef);
+
+
+    if (!userSnap.exists()) {
+      await setDoc(userRef, {
+        uid: user.uid,
+        name: user.displayName,
+        email: user.email,
+        photoURL: user.photoURL,
+        gamesPlayed: 0,
+        wins: 0,
+        losses: 0,
+        createdAt: new Date()
+      });
+
+      console.log('New user created in Firestore');
+    } else {
+      console.log('User already exists');
+    }
+
+    console.log('Logged in:', user);
+  }
+}
